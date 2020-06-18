1ec0fe4d9f812b37e2a0d99efb6b11c979590ca2
Raphael Bandeira <rbandeir@gmail.com>
2020-06-18 16:53:55 -0300

    Adicionar arquivos de projeto.

---
+++ ModelBaraTIao/Bairro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelBaraTIao
+{
+    class Bairro
+    {
+
+        public int Id { get; set; }
+
+        public int Descricao { get; set; }
+
+    }
+}
---
+++ ModelBaraTIao/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelBaraTIao
+{
+    class Categoria
+    {
+
+        public int Id { get; set; }
+
+        public int Descricao { get; set; }
+
+        public int? IdCategoriaPai { get; set; }
+
+        public Categoria CategoriaPai { get; set; }
+    }
+}
